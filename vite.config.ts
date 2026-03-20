1f14610f3b55f4572d944752f8848a700a37f397
AlexDacorte <dacortejhexson@gmail.com>
2026-03-20 09:26:18 -0500

    added corrected logo version

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/pronto_pizza_almagro",
   server: {
     host: "::",
     port: 8080,
